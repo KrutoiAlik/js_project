9cb5142dd68d66bbaa01de83dbf50c1556728016
bezmoshyk@yandex.ru <Alex_20th>
2018-03-15 00:08:01 +0300

    var and const

---
+++ new/scripts/VarAndConst.js
@@ -0,0 +1,11 @@
+var value = 5;
+
+var CONST_VALUE = 10;
+CONST_VALUE = 1; // серьезно, нет никакой фиксации константы, даже FINAL не сделают
+
+//Задания
+var admin;
+var name;
+name = 'Василий';
+admin = name;
+alert(admin);
