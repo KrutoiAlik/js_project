d29222c668b7ef86b9828a1f861e83e2d5f1f4b8
bezmoshyk@yandex.ru <Alex_20th>
2018-03-24 23:17:43 +0300

    variables and types of

--- new/scripts/VarAndConst.js
+++ new/scripts/VarAndConst.js
@@ -1,8 +1,33 @@
 var value = 5;
+value = 12.33;
+value = 'String';
+
+value = false;
+if (value) {
+    alert('True');
+} else {
+    alert('False');
+}
+
+var empty;
+
+alert(empty);
+
+alert(1/0);
+
+var obj = {
+    name: 'Имя',
+    age: 15,
+    sex: 'Male'
+}
+
+alert(obj.name);
 
 var CONST_VALUE = 10;
 CONST_VALUE = 1; // серьезно, нет никакой фиксации константы, даже FINAL не сделают
-
+alert(CONST_VALUE);
+CONST_VALUE = 2;
+alert(CONST_VALUE);
 //Задания
 var admin;
 var name;
